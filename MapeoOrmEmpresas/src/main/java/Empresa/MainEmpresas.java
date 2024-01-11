ba7dd63811bf65ba48369b10b477903b2e34a14a
xjosejurado21 <115066237+xjosejurado21@users.noreply.github.com>
2024-01-11 11:56:59 +0100

    Ejercicio2

---
+++ MapeoOrmEmpresas/src/main/java/Empresa/MainEmpresas.java
@@ -0,0 +1,65 @@
+package Empresa;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.context.internal.ThreadLocalSessionContext;
+import org.hibernate.engine.spi.SessionFactoryImplementor;
+
+
+public class MainEmpresas {
+
+
+	public static void main(String[] args) {
+
+
+	    // Configurar la sesi�n de Hibernate
+	    SessionFactory sessionFactory = new Configuration()
+	    	   .configure()
+
+
+	          //  .configure("hibernate.cfg.xml") // Ruta del archivo de configuraci�n de Hibernate
+	            .buildSessionFactory(); // Construir la sesi�n de Hibernate
+
+
+	    // Configurar la sesi�n en el contexto actual
+	    ThreadLocalSessionContext context = new ThreadLocalSessionContext((SessionFactoryImplementor) sessionFactory);
+	    context.bind(sessionFactory.openSession());
+
+
+	    try {
+	        // Crear objeto cliente
+	        Clientes cliente = new Clientes("Eva", "Sevilla");
+
+
+	        // Obtener la sesi�n actual
+	        Session session = context.currentSession();
+
+
+	        // Iniciar transacci�n
+	        session.beginTransaction();
+
+
+	        // Guardar objeto en la base de datos
+	        session.save(cliente);
+
+
+	        // Hacer commit de la transacci�n
+	        session.getTransaction().commit();
+
+
+	        // Imprimir cliente guardado en la base de datos
+	        System.out.println("Clientes guardado: " + cliente);
+
+
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    } finally {
+	        // Desligar la sesi�n del contexto
+	        ThreadLocalSessionContext.unbind(sessionFactory);
+	        // Cerrar la sesi�n de Hibernate
+	        sessionFactory.close();
+	    }
+	}
+
+
+}
